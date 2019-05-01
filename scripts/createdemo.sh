5ac4ff43740a530310372b1cae3b33e4c2077eb2
purocean <purocean@gmail.com>
2019-05-01 17:53:26 +0800

    update

--- scripts/createdemo.sh
+++ scripts/createdemo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd microservice-demo
-docker run -it --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-11-slim mvn package
+docker run -it --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-11-slim mvn -s setting.xml -Dmaven.test.skip=true package
 cd ../docker
 docker-compose build
